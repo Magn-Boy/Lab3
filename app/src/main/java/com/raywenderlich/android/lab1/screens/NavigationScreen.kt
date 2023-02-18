34c6838a695be5dfa7532164129e39470eaa0f4c
AngiOnill <93878632+Magn-Boy@users.noreply.github.com>
2023-02-19 01:35:03 +0300

    add button myColumn. Changed file strings.xml

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -35,6 +35,7 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.button_progress), Screen.ProgressIndicator)
             NavigationButton(stringResource(id = R.string.button_alertDialog), Screen.AlertDialog)
             NavigationButton(stringResource(id = R.string.rowScreen), Screen.Row)
+            NavigationButton(stringResource(id = R.string.myColumn), Screen.Column)
         }
     }
 }
